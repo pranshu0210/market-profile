e4518d7b4f859109c3f528e4f58303dff9bc6d59
Pranshu Dave <pranshudave@gmail.com>
2018-01-05 23:06:21 +0530

    Add necessary comments in example.py

--- example.py
+++ example.py
@@ -2,15 +2,27 @@ import ccxt
 
 from mktProfile import MarketProfile
 
+'''
+An example on how to use this script.
+Here, we are retrieving 1 minute ohlcv data from Bittrex. 
+'''
+
+# Initialize Exchange
 bittrex = ccxt.bittrex()
 bittrex.load_markets(reload=True)
 
+# Define Symbol
 symbol = 'BTC/USDT'
+
+# Fetch Ohlcv data
 ohlcv = bittrex.fetch_ohlcv(symbol, "1m")
 period = 6 * 60 * 60 * 1000
 
+# Create an object of MarketProfile
 mktPfl = MarketProfile(ohlcv_list=ohlcv, duration=period)
 mktPfl.prepare_ohlcv()
+
+# Build the profile
 df = mktPfl.create_profile()
 
 print(df)
