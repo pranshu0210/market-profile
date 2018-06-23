0512ed53051928f7ec92bfd08d1e7c84e497c6e3
Pranshu Dave <pranshudave@gmail.com>
2018-06-23 09:08:21 +0530

    Add functionality to reduce profile into a selected number of rows

--- mktProfile/__init__.py
+++ mktProfile/__init__.py
@@ -18,6 +18,7 @@ class MarketProfile:
         self.cpt_market_profile = None
         self.my_list = None
         self.cmp_df = None
+        self.u_step_df = None
 
         self.convert_to_list()
 
@@ -70,7 +71,7 @@ class MarketProfile:
         """
         self.ohlcv_list = list(self.prepare(self.ohlcv_list, self.duration))
 
-    def create_profile(self):
+    def create_profile(self, ones=False):
         """
         Creates the market profile
         :return: market profile in the form of a pandas Dataframe
@@ -129,12 +130,12 @@ class MarketProfile:
         mychar = 65
         # Fill in empty cell belonging to the same interval
         for j in range(1, len(self.my_list[0])):
-            first_idx = 0
+            first_idx = -1
             last_idx = 0
 
             for i in range(0, len(self.my_list)):
                 if self.my_list[i][j] == chr(mychar):
-                    if first_idx == 0:
+                    if first_idx == -1:
                         first_idx = i
                     last_idx = i
 
@@ -145,17 +146,26 @@ class MarketProfile:
             mychar += 1
 
         self.df = pd.DataFrame(self.my_list)
+        if ones is True:
+            self.df = self.df.replace('[A-Z]', 1, regex=True)
         return self.df
 
-    def compact_profile(self):
+    def compact_profile(self, new_df=None):
         """
         Compacts the standard market profile
         :return: 
         """
-        if self.df is None:  # Create profile if not already created
-            self.create_profile()
+        uncmpt_list = []
+        if new_df is None:
+            uncmpt_df = self.df
+            if uncmpt_df is None:  # Create profile if not already created
+                self.create_profile()
+            uncmpt_list = self.my_list
+        else:
+            uncmpt_list = new_df.values.tolist()
+
         cmp_list = []
-        for row in self.my_list:
+        for row in uncmpt_list:
             cmp_list.append(list(filter(lambda a: a != 0, row)))
         self.cmp_df = pd.DataFrame(cmp_list)
         return self.cmp_df
@@ -207,3 +217,40 @@ class MarketProfile:
         if self.df is None:
             self.create_profile()
         return self.low, self.high
+
+    def reduce_profile(self, n_r, compact=False):
+        """
+        Reduces the profile to the given number of rows
+        :param n_r: number of rows to reduce the profile to
+        :return:
+        """
+        n_r = n_r - 1  # To account for the fact that the starting and ending prices will also be included
+        price_range = self.range()  # Get the range
+        step = (price_range[1] - price_range[0]) / n_r
+
+        # Store the prices in the form of steps
+        price_list = [i * step + price_range[0] for i in range(0, n_r + 1)]
+
+        # Get the actual list of prices from dataframe
+        price_list_actual = self.df.ix[:, 0].tolist()
+
+        # Find the indices closest rows
+        price_idx = []
+        for j in range(0, len(price_list)):
+            price_idx.append(
+                min(range(len(price_list_actual)), key=lambda i: abs(price_list_actual[i] - price_list[j])))
+
+        # Get the rows only associated with indices in price_idx
+        self.u_step_df = self.df.iloc[price_idx, :]
+
+        # Replace first column with price list
+        self.u_step_df[0] = price_list
+
+        # Reorder the indices
+        self.u_step_df.index = range(len(price_list))
+        # Reverse the dataframe
+        self.u_step_df = self.u_step_df.iloc[::-1]
+        self.u_step_df.index = range(len(price_list))
+        if compact:
+            return self.compact_profile(new_df=self.u_step_df)
+        return self.u_step_df
